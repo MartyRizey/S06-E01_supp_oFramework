238837765a15ca5c4b8fcbc646afa09e0df93e4f
MartyRizey <martyrizey@gmail.com>
2018-11-09 12:20:42 +0100

    correction FrontController mis  à vide

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # S06-E01_supp_oFramework
 
 Support pour réviser l'architecture MVC dans le cadre d'un framework
+- Initial commit
+- First commit
+  + création README.MD
+- Ajout architecture + .htaccess
+  + ajout de fichier .gitkeep dans les dossiers vides
+- Schéma architecture de base
+- Ajout dépendances via 'composer' ou packagist.org
+  + altorouter
+  + var-dumper
+- Schéma dépendances
+- Modifs schéma dépendances + ajout pdf correspondant
+- Point d'entrée + frontController + schéma
+- Suite FrontController
 
--- app/Application.php
+++ app/Application.php
@@ -3,16 +3,6 @@
 // Mise en place d'un FrontController séparé de notre point d'entrée (index.php)
 class Application
 {
-    // https://www.grafikart.fr/tutoriels/php/router-628
-    // Je créé un attribut privé qui va contenir mon router
-    private $router;
-
-    // ma fonction __construct est une fonction dite magique
-    // celle-ci est automatiquement appelée lorsque ma classe est instanciée
-    // autrement dit lorsque l'on fait un new Application()
-    public function __construct()
-    {
-        
-    }
     
 }
+    
